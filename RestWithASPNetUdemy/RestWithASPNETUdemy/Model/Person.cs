c3bfdfee2441a0c36d6274fa1b32eb4785b5c698
Mikaelly Carneiro <mikaellyscarneiro@gmail.com>
2023-08-07 11:48:17 -0300

    Implementando a Interface IPersonService

---
+++ RestWithASPNetUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        public long id { get; set; }    
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Adress { get; set; }
+        public string Gender { get; set; }
+    }
+}
